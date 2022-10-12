42b89a149bbd69cca6cd2da8844808deb5ab1a5e
Charonzhou <1967279919@qq.com>
2022-10-12 10:10:22 +0100

    Site updated: 2022-10-12 10:10:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2022/10/06/About-Linux-Network/","2022/10/08/about-centos-netcfg/","2022/10/11/Query-system-version/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2022/10/06/About-Linux-Network/","2022/10/08/about-centos-netcfg/","2022/10/11/Query-system-version/","2022/10/11/about-centos-SSH/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
